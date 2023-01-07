538f4892f340581442aa885ff0fd6b8efd1a94d8
RLjumber <113863686+RLjumber@users.noreply.github.com>
2023-01-06 20:08:38 -0500

    I added the post titles, havent messed with the find module exported from postBank.js however. I added the static html within the new route to host the post titles. Some comments with more details within the the main app.js file.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const postBank = require("./postBank")
 
 const morgan = require("morgan");
 app.use(morgan("dev"));
@@ -7,7 +8,35 @@ app.use(morgan("dev"));
 
 
 
-app.get("/", (req, res) => res.send("<h1>WIZARDS ONLY FOOLS!!!</h1><h2>Testing</h2>"));
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  // postBank.list grabs our list module frome the module exports
+
+  const html = `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wiz News</title>
+        <link rel="stylesheet" href="style.css">
+      </head>
+
+      <body>
+        <ul>
+          ${posts.map(post => 
+            `<li className="post_title">${post.title}
+                <p>By: ${post.name}</p>
+            </li>`
+          )}
+        </ul>
+      </body>
+    </html>
+  `
+  // cannot get the styling to apply however
+  // There is some weird concatenation within the <ul>, different than JSX but similar kinda style with the html within a js and we can use methods like .map inside them. cool stuff.
+  // List is comma separated however there are MDN docs on it, still needs to be done. I added a class to the list items so we can add links to them later on or for now i want them to color when hovered.
+
+  res.send(html);
+})
+
 
 const PORT = 1337;
 
